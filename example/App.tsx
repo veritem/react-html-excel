adbd890dc4f5430669f7f83e6b36ab90805fec1c
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-04-24 19:40:42 +0200

    feat: initial commit

---
+++ example/App.tsx
@@ -0,0 +1,33 @@
+import ReacthtmlToexcel from "@veritem/react-html-excel";
+import React from "react";
+
+export function App() {
+  return (
+    <section>
+      <ReacthtmlToexcel
+        className="download-table-xls-button"
+        table="table-to-xls"
+        filename="tablexls"
+        sheet="tablexls"
+        buttonText="Download as XLS"
+      />
+      <table id="table-to-xls">
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+      </table>
+    </section>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+interface IProps {
+  table: string;
+  filename: string;
+  sheet: string;
+  className: string;
+  buttonText: string;
+}
+
+export default function HtmlToExecl({
+  table,
+  filename,
+  sheet,
+  className = "button-download",
+  buttonText = "Download",
+}: IProps) {
+  const [tableState] = useState(table);
+  const [filenameState] = useState(filename);
+  const [sheetState] = useState(sheet);
+
+  function base64(s) {
+    return window.btoa(encodeURIComponent(s));
+  }
+
+  function format(s, c) {
+    return s.replace(/{(\w+)}/g, (m, p) => c[p]);
+  }
+
+  function handleDownload() {
+    if (!document) {
+      if (process.env.NODE_ENV !== "production") {
+        console.error("Failed to access document object");
+      }
+      return;
+    }
+
+    if (
+      document.getElementById(tableState).nodeType !== 1 ||
+      document.getElementById(tableState).nodeName !== "TABLE"
+    ) {
+      if (process.env.NODE_ENV !== "production") {
+        console.error("Provided table property is not html table element");
+      }
+
+      return;
+    }
+
+    const table = document.getElementById(tableState).outerHTML;
+    const sheet = String(sheetState);
+    const filename = `${String(filenameState)}.xlsx`;
+    const uri = "data:application/vnd.ms-excel;base64,";
+    const template =
+      '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-mic' +
+      'rosoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta cha' +
+      'rset="UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:Exce' +
+      "lWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>" +
+      "</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></" +
+      "xml><![endif]--></head><body>{table}</body></html>";
+
+    const context = {
+      worksheet: sheet || "Worksheet",
+      table,
+    };
+
+    const element = document.createElement("a");
+    element.href = uri + base64(format(template, context));
+    element.download = filename;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+
+  return (
+    <button className={className} type="button" onClick={handleDownload}>
+      {buttonText}
+    </button>
+  );
+}
