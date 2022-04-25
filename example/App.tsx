74b21d0245d26b36e124e137fb0c5dc2e3506a09
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-04-25 16:49:00 +0200

    fix: react deps reducing bundle size

--- example/App.tsx
+++ example/App.tsx
@@ -6,32 +6,29 @@ export function App() {
     <section>
       <ReacthtmlToexcel
         className="download-table-xls-button"
-        table="table-to-xls"
+        table="table"
         filename="table"
-        sheet="tablexls"
-        buttonText="Download as XLS"
+        sheet="table"
+        extension="xls"
+        buttonText="Download"
       />
-      <table id="table-to-xls">
-        <thead>
-          <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Age</th>
-          </tr>
-        </thead>
+      <table id="table">
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
 
-        <tbody>
-          <tr>
-            <td>Jill</td>
-            <td>Smith</td>
-            <td>50</td>
-          </tr>
-          <tr>
-            <td>Eve</td>
-            <td>Jackson</td>
-            <td>94</td>
-          </tr>
-        </tbody>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
       </table>
     </section>
   );
